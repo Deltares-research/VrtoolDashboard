54bf01c7086a77e65a61a0be98a5788d41a54359
MatthiasHauth <matthias.hauth@deltares.nl>
2024-02-07 11:32:33 +0100

    fix tests

--- tests/test_plotly_figures/test_pf_cost_figure.py
+++ tests/test_plotly_figures/test_pf_cost_figure.py
@@ -13,14 +13,14 @@ from src.plotly_graphs.pf_length_cost import plot_pf_length_cost
 class TestPlotlyScatter:
     @pytest.mark.parametrize("result_type", [ResultType.RELIABILITY, ResultType.PROBABILITY])
     @pytest.mark.parametrize("cost_length_switch", ["COST", "LENGTH"])
-    def test_plot_pf_length_cost(self, result_type: Enum, cost_length_switch: str):
+    def test_plot_pf_length_cost_economic_optimum(self, result_type: Enum, cost_length_switch: str):
         # 1. Define data
         _dike_data = json.load(
             open(Path(__file__).parent.parent / 'data/Case_38_1_sterker_VZG2/reference' / 'dike_data.json'))
         _dike_traject = DikeTraject.deserialize(_dike_data)
 
         # 2. Call
-        _fig = plot_pf_length_cost(_dike_traject, 2025, result_type.name, cost_length_switch)
+        _fig = plot_pf_length_cost(_dike_traject, 2025, result_type.name, cost_length_switch, "ECONOMICAL_OPTIMUM", None)
 
         # 3. Assert
         assert isinstance(_fig, Figure)
