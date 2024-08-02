b8c74cb1bc968a69c046dd1ac7ee5ba924fc281c
MatthiasHauth <matthias.hauth@deltares.nl>
2024-08-02 15:05:53 +0200

    fix

--- src/orm/importers/custom_measures_importer.py
+++ src/orm/importers/custom_measures_importer.py
@@ -2,7 +2,7 @@ from vrtool.common.enums import CombinableTypeEnum
 from vrtool.defaults.vrtool_config import VrtoolConfig
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
 from vrtool.orm.models import CustomMeasureDetail, Measure, Mechanism, MechanismPerSection, SectionData
-
+from src.constants import Mechanism as MechanismEnum
 
 class CustomMeasureImporter(OrmImporterProtocol):
 
@@ -32,11 +32,22 @@ class CustomMeasureImporter(OrmImporterProtocol):
             _mechanism = Mechanism().select().where(Mechanism.id == _mechanism_per_section.mechanism_id).get()
             _measure = Measure().select().where(Measure.id == cm_detail.measure_id).get()
 
+            if _mechanism.name == "Piping":
+                meca = MechanismEnum.PIPING.value
+            elif _mechanism.name == "Overflow":
+                meca = MechanismEnum.OVERFLOW.value
+            elif _mechanism.name == "StabilityInner":
+                meca = MechanismEnum.STABILITY.value
+            elif _mechanism.name == "Revetment":
+                meca = MechanismEnum.REVETMENT.value
+            else:
+                raise ValueError(f"Mechanism {_mechanism.name} is not recognized")
+
             _cm_data = dict(beta=cm_detail.beta,
                             cost=cm_detail.cost,
                             time=cm_detail.year,
                             section_name=_section.section_name,
-                            mechanism=_mechanism.name,
+                            mechanism=meca,
                             measure_name=_measure.name,
                             # COMBINABLE_TYPE_TYPE=CombinableTypeEnum.FULL.name,
                             )
