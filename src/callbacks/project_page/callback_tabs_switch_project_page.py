50b80e79097cd13100415c1d56eed17d92bb4652
MatthiasHauthDeltares <113418841+MatthiasHauthDeltares@users.noreply.github.com>
2024-09-03 14:03:53 +0200

    2 main tabs for project page

--- src/callbacks/project_page/callback_tabs_switch_project_page.py
+++ src/callbacks/project_page/callback_tabs_switch_project_page.py
@@ -1,25 +1,27 @@
-from dash import callback, Output, Input, html
+from dash import callback, Output, Input, html, State
 
 from src.layouts.layout_project_page.layout_project_definition_tab import project_definition_tab_layout
 
 
 @callback(
-
-    Output("content_tab_project_page", "children"),
-
-    [Input("tabs_tab_project_page", "active_tab")],
+    [Output("content_tab_project_page", "children")],
+    [Input("tabs_tab_project_page", "value")],
 )
-def render_tab_project_page(active_tab: str) -> html.Div:
-    """
-    Renders the content of the selected tab for the project page.
-    :param active_tab:
-    :return:
-    """
-    if active_tab == "tab-111":
-        return project_definition_tab_layout
-
-    if active_tab == "tab-112":
-        return html.Div("Not yet implemented")
-
-    else:
-        return html.Div("Invalid tab selected")
+def render_tab_content(tab_switch):
+    if tab_switch == "tab-111" or tab_switch == "tab-1":
+        return [project_definition_tab_layout]
+    if tab_switch == "tab-112" or tab_switch == "tab-2":
+        return [html.Div("Not yet implemented")]
+    return [html.Div("Not yet implemented")]
+    # return html.Div(
+    #     id="status-container",
+    #     children=[
+    #         # build_quick_stats_panel(),
+    #         html.Div("Not yet implemented"),
+    #         html.Div(
+    #             id="graphs-container",
+    #             # children=[build_top_panel(stopped_interval), build_chart_panel()],
+    #             children=[html.Div("Not yet implemented"), html.Div("Not yet implemented")],
+    #         ),
+    #     ],
+    # )
---
+++ src/layouts/layout_project_page/layout_project_visualization_tab.py
@@ -0,0 +1,10 @@
+from dash import html
+import dash_bootstrap_components as dbc
+
+
+project_definition_tab_layout = html.Div([
+    dbc.Row([
+        dbc.Col([], md=5),
+        dbc.Col([], md=7),
+    ]),
+])
