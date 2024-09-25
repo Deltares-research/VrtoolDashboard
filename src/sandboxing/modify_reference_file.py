953c0d125389de53ac2bfae21cc9d216b99962ab
MatthiasHauthDeltares <113418841+MatthiasHauthDeltares@users.noreply.github.com>
2024-09-25 17:26:07 +0200

    update fixtures

--- src/sandboxing/modify_reference_file.py
+++ src/sandboxing/modify_reference_file.py
@@ -1,21 +1,28 @@
 from pathlib import Path
 import json
 # path_to_reference_file = Path(r"C:\Users\hauth\bitbucket\VRtoolDashboard\tests\data\Case_31_1\reference\reference_dike_traject.json")
-path_to_reference_file = Path(r"C:\Users\hauth\bitbucket\VRtoolDashboard\tests\data\Case_38_1_sterker_VZG2\reference\dike_data.json")
+path_to_reference_file = Path(r"C:\Users\hauth\bitbucket\VRtoolDashboard\tests\data\TestCase1_38-1_no_housing\reference\dike_traject_data.json")
 
 with open(path_to_reference_file, "r") as file:
     reference = json.load(file)
 
-for section in reference['dike_sections']:
 
-    mechanisms = []
-    if section.get("initial_assessment", {}) is None:
-        section['active_mechanisms'] = []
-        continue
-    for key in section.get("initial_assessment", {}).keys():
-        if key != "Section":
-            mechanisms.append(key)
-    section['active_mechanisms'] = mechanisms
+########## 1 #############
+# for section in reference['dike_sections']:
+#
+#     mechanisms = []
+#     if section.get("initial_assessment", {}) is None:
+#         section['active_mechanisms'] = []
+#         continue
+#     for key in section.get("initial_assessment", {}).keys():
+#         if key != "Section":
+#             mechanisms.append(key)
+#     section['active_mechanisms'] = mechanisms
+
+########## 2 #############
+for section in reference["dike_sections"]:
+    if section.get("flood_damages", {}) is None:
+        section["flood_damages"] = 0
 
 # Write the modified reference file
 with open(path_to_reference_file, "w") as file:
