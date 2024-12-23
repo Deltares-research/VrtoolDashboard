0017219c8a2a77d9805a3e7dc066cc9f8ddd4c8e
hauth <matthias.hauth@deltares.nl>
2024-12-23 14:19:02 +0100

    sandbox

--- src/sandboxing/project_page/sandbox_project_map_result.py
+++ src/sandboxing/project_page/sandbox_project_map_result.py
@@ -1,19 +1,28 @@
 import json
 from pathlib import Path
 
-from src.linear_objects.reinforcement_program import get_projects_from_saved_data
+from src.constants import ResultType
+from src.linear_objects.reinforcement_program import get_projects_from_saved_data, DikeProgram
 from src.plotly_graphs.project_page.plotly_maps import plot_project_overview_map
+from src.plotly_graphs.project_page.plotly_plots import projects_reliability_over_time
 
-
-saved_data_path = Path(r"N:\Projects\11209000\11209353\C. Report - advise\Handreiking\voorbeeld_programmering\Programmering WDOD.json")
+saved_data_path = Path(r"C:\Users\hauth\OneDrive - Stichting Deltares\Documents\tempo\VRM\bug_area\41-2 urgentie.json")
 data = json.load(open(saved_data_path))
 print(data.keys())
 
 _imported_runs_data = data['imported_runs_data']
 _projects_overview_data = data['project_data']
 
-_projects, _ = get_projects_from_saved_data(_imported_runs_data, _projects_overview_data)
+# _projects, _ = get_projects_from_saved_data(_imported_runs_data, _projects_overview_data)
+#
+# _fig = plot_project_overview_map(_projects)
+# _fig.show()
 
-# 2. Call
-_fig = plot_project_overview_map(_projects)
+
+
+
+program = DikeProgram(_imported_runs_data, _projects_overview_data)
+
+        # 2. Call
+_fig = projects_reliability_over_time(program, ResultType.RELIABILITY.name)
 _fig.show()
